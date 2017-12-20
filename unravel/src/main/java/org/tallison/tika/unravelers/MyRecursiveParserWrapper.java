d351eaf2132c899e3ffdaf2766af1e5a78a4885f
tballison <tallison@mitre.org>
2017-12-20 08:43:42 -0500

    update 1.17 -- remove recursive parser wrapper built for earlier versions of Tika

--- unravel/src/main/java/org/tallison/tika/unravelers/MyRecursiveParserWrapper.java
+++ unravel/src/main/java/org/tallison/tika/unravelers/MyRecursiveParserWrapper.java
@@ -1,6 +1,3 @@
-package org.tallison.tika.unravelers;
-
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -17,6 +14,7 @@ package org.tallison.tika.unravelers;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.tallison.tika.unravelers;
 
 import java.io.IOException;
 import java.io.InputStream;
