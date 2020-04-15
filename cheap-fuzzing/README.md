98a2b60f808b4bfead180f0f9756fed1bec692c4
tallison <tallison@apache.org>
2020-04-15 08:37:18 -0400

    add fuzzing module

---
+++ cheap-fuzzing/README.md
@@ -0,0 +1,8 @@
+#Goals
+This module allows for rudimentary fuzzing of files.  If you're looking for something fancy, please 
+check with the experts.  You are looking in the wrong place.
+
+* https://github.com/rohanpadhye/JQF
+* https://github.com/fuzzitdev/javafuzz
+* https://people.csail.mit.edu/akiezun/jfuzz/documentation.html
+* https://www.floyd.ch/?p=1090
---
+++ cheap-fuzzing/src/main/java/org/tallison/tika/fuzzing/ByteFlipper.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tallison.tika.fuzzing;
+
+import java.io.IOException;
+import java.util.Random;
+
+public class ByteFlipper implements Transformer {
+
+    private Random random = new Random();
+    private float percentCorrupt = 0.1f;
+    //TODO add something about protecting first x bytes?
+
+    @Override
+    public byte[] transform(byte[] input) throws IOException {
+        byte[] corrupted = new byte[input.length];
+        byte[] singleByte = new byte[1];
+        for (int i = 0; i < input.length; i++) {
+            if (random.nextFloat() <= percentCorrupt) {
+                random.nextBytes(singleByte);
+                corrupted[i] = singleByte[0];
+            } else {
+                corrupted[i] = input[i];
+            }
+        }
+        return corrupted;
+    }
+
+    public void setPercentCorrupt(float percentCorrupt) {
+        percentCorrupt = percentCorrupt;
+    }
+}
